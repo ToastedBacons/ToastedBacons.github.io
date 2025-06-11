f44c8db01e2e5381e109f3d6bc02ff1a3a1d8a60
Shawn Lin <mrshawnlin0807@gmail.com>
2025-06-11 19:15:10 -0400

    e

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function searchArticles() {
   const query = document.getElementById('search').value;
-  document.getElementById('results').innerHTML = `Searching for: <b>${query}</b> (connect API here)`;
+    document.getElementById('results').innerHTML = `Searching for: <b>${query}</b> (connect API here)`;
+
+}
+document.addEventListener('keyup' (event) => { }){
+    if (event.key === 'Enter'){
+        searchArticles();
+    }
 }
