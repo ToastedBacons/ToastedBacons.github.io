5a0232cdd21262e82d0e734eea21b1e0d063ad12
Shawn Lin <mrshawnlin0807@gmail.com>
2025-06-12 23:42:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function searchArticles() {
   const query = document.getElementById('search').value;
   document.getElementById('results').innerHTML = `Searching for: <b>${query}</b> (connect API here)`;
+  searchAPI(query);
 }
 
 document.addEventListener('keyup', (event) => {
@@ -8,3 +9,13 @@ document.addEventListener('keyup', (event) => {
     searchArticles();
   }
 });
+async function searchAPI(query){
+  try{
+    const response = await fetch(`https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=${encodeURIComponent(query)}&format=json`);
+    const output = await response.json();
+    console.log(output);
+  } catch(error){
+    console.error("API error", error)
+  }
+
+}
